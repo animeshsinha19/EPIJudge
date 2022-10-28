33950d57a4d52bf1c8c32527ea5a04787d30fe1f
Animesh Sinha <animeshsinha19@gmail.com>
2022-10-28 00:30:53 -0400

    parity using XORs associative property

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -13,14 +13,26 @@ public class Parity {
   //   return result;
   // }
 
+  // unset least significant set bit method
+  // @EpiTest(testDataFile = "parity.tsv")
+  // public static short parity(long x) {
+  //   short result = 0;
+  //   while(x != 0) {
+  //     result ^= 1;
+  //     x &= (x - 1);
+  //   }
+  //   return result;
+  // }
+
+  // using XOR's associative property
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    short result = 0;
-    while(x != 0) {
-      result ^= 1;
-      x &= (x - 1);
+    int denominator = Long.SIZE / 2;
+    while (denominator != 0) {
+      x ^= x >>> denominator;
+      denominator /= 2;
     }
-    return result;
+    return (short) (x & 0x1);
   }
 
   public static void main(String[] args) {
